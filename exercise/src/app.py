746c41480c1b502c731dd03cd529d5c7166ba3a9
Michiel van Heusden <m.vanheusden@basicit.nl>
2026-03-12 16:23:57 +0100

    updated ignore

--- exercise/src/app.py
+++ exercise/src/app.py
@@ -5,3 +5,14 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+
+# this is an endpoint that yields the message "goodbye world" 
+@app.get("/goodbye")
+async def goodbye():
+    return { "message": "Goodbye World" }
+
+
+@app.post("/echo")
+async def echo(message: str):
+    return {"message": message}
